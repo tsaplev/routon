d7e17b0e574449607d24057e12808c4437b16f21
Alexander Tsaplev <alexander@tsaplev.me>
2021-03-14 16:54:44 +0300

    Create Table component

--- src/components/Route/Table.tsx
+++ src/components/Route/Table.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteRoute, deleteAllRoutes, selectRoutes } from './routeSlice';
 
 export function Table() {
-  return <div>Table Component</div>;
+  const routes = useSelector(selectRoutes);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="table dbg-box">
+      <h2>Table</h2>
+      {routes.map((el: any) => {
+        const route = {
+          id: el.id,
+          from: el.from.name,
+          to: el.to.name,
+          by: el.transport,
+        };
+
+        return (
+          <pre key={route.id} style={{ background: 'lightgray' }}>
+            <p>{JSON.stringify(route, null, 2)}</p>
+          </pre>
+        );
+      })}
+      <button onClick={() => dispatch(deleteRoute())}>Delete</button>
+      <button onClick={() => dispatch(deleteAllRoutes())}>Delete all</button>
+    </div>
+  );
 }

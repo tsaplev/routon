7e88b8e33d9f5adb371ec8322ed2ac94b5a4e8ff
Alexander Tsaplev <alexander@tsaplev.me>
2021-03-14 16:53:48 +0300

    Create default state for routes

---
+++ src/constants/routes.ts
@@ -0,0 +1,46 @@
+import { Route } from '../app/types';
+
+const routes: Route[] = [
+  {
+    id: 'nYw34AOsYpJG5BXHfXQzx',
+    departure: '2019-10-16 01:10:00',
+    arrival: '2019-10-16 07:30:00',
+    from: {
+      name: 'Moscow',
+      lat: 55.7558,
+      lon: 37.6173,
+    },
+    to: {
+      name: 'Dubai',
+      lat: 25.2048,
+      lon: 55.2708,
+    },
+    transport: 'plane',
+    path: [
+      { lat: 55.7558, lon: 37.6173 },
+      { lat: 25.2048, lon: 55.2708 },
+    ],
+  },
+  {
+    id: 'Sy0BEfPNO3EiyUuzc2x6U',
+    departure: '2019-10-16 01:10:00',
+    arrival: '2019-10-16 07:30:00',
+    from: {
+      name: 'Dubai',
+      lat: 55.7558,
+      lon: 37.6173,
+    },
+    to: {
+      name: 'Manila',
+      lat: 25.2048,
+      lon: 55.2708,
+    },
+    transport: 'plane',
+    path: [
+      { lat: 55.7558, lon: 37.6173 },
+      { lat: 25.2048, lon: 55.2708 },
+    ],
+  },
+];
+
+export { routes };

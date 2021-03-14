daa8985ab36698f3ebbbc2249342f914d8b621a5
Alexander Tsaplev <alexander@tsaplev.me>
2021-03-14 16:55:15 +0300

    Create Map component

--- src/components/Route/Map.tsx
+++ src/components/Route/Map.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectRoutes } from './routeSlice';
 
 export function Map() {
-  return <div>Map Component</div>;
+  const routes = useSelector(selectRoutes);
+  return (
+    <div className="dbg-box">
+      <h2>Map</h2>
+      {routes.map((route) => {
+        return <pre key={route.id}>{JSON.stringify(route.path)}</pre>;
+      })}
+    </div>
+  );
 }

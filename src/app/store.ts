d0713d0b1fa9ec13261fad0086af8e9dadf01589
Alexander Tsaplev <alexander@tsaplev.me>
2021-03-14 16:55:28 +0300

    Add route reducure to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import routeReducer from '../components/Route/routeSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    route: routeReducer,
   },
 });
 

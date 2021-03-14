b19ceaed0f4692153a9a46ccf93c92b5d111c7f4
Alexander Tsaplev <alexander@tsaplev.me>
2021-03-14 16:58:15 +0300

    Remove default Counter component

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import routeReducer from '../components/Route/routeSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     route: routeReducer,
   },
 });

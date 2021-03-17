32e8120b8bdb70dc8b6d6532fb103bb8267dd7af
Alexander Tsaplev <alexander@tsaplev.me>
2021-03-17 22:33:52 +0300

    Add try/catch block for parsing

--- src/RouteCalculator.ts
+++ src/RouteCalculator.ts
@@ -7,35 +7,40 @@ class RouteCalculator {
     to: MapPoint,
     vehicle: Vehicle
   ): Promise<MapPath> {
-    let path = [from, to];
+    const path = [from, to];
     const { travelMode } = transportList[vehicle];
 
-    if (travelMode !== 'OTHER') {
-      return new Promise((resolve, reject) => {
-        const directionsService = new google.maps.DirectionsService();
-        const request = {
-          origin: from,
-          destination: to,
-          travelMode: google.maps.TravelMode[travelMode],
-        };
+    if (travelMode === 'OTHER') {
+      return path;
+    }
 
-        directionsService.route(request, (response, status) => {
-          if (status !== 'OK') {
-            console.error('Directions request failed due to ' + status);
-            resolve(path);
-          }
+    return new Promise((resolve, reject) => {
+      const directionsService = new google.maps.DirectionsService();
+      const request = {
+        origin: from,
+        destination: to,
+        travelMode: google.maps.TravelMode[travelMode],
+      };
 
+      directionsService.route(request, (response, status) => {
+        if (status !== 'OK') {
+          console.error('Directions request failed due to:' + status);
+          return reject(path);
+        }
+
+        try {
           const overviewPath = response.routes[0].overview_path;
           const linePath = overviewPath.reduce((acc: MapPath, point: any) => {
             return [...acc, { lat: point.lat(), lng: point.lng() }];
           }, []);
 
-          resolve(linePath);
-        });
+          return resolve(linePath);
+        } catch (e) {
+          console.error(e);
+          return path;
+        }
       });
-    }
-
-    return [from, to];
+    });
   }
 }
 

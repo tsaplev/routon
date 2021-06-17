0e880c3dc0c39b146770daba216b5219ba2f68aa
Alexander Tsaplev <a.tsaplev@corp.mail.ru>
2021-06-17 08:32:13 +0300

    refactor

--- src/components/Route/Map.tsx
+++ src/components/Route/Map.tsx
@@ -1,75 +1,88 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Route } from '../../app/types';
 import { selectRoutes } from './routeSlice';
 
 export function Map() {
   const routes = useSelector(selectRoutes);
+  const [map, setMap] = useState({});
 
   useEffect(() => {
-    function initMap(): void {
-      console.log(routes);
-      const map = new google.maps.Map(
-        document.getElementById('map') as HTMLElement,
-        {
-          zoom: 2.3,
-          center: { lat: 50, lng: 80 },
-          mapTypeId: 'terrain',
-          disableDefaultUI: true,
-        }
-      );
+    const map = new google.maps.Map(
+      document.getElementById('map') as HTMLElement,
+      {
+        zoom: 2.3,
+        center: { lat: 50, lng: 80 },
+        mapTypeId: 'terrain',
+        disableDefaultUI: true,
+        zoomControl: true,
+      }
+    );
 
-      const cirlce = {
-        path: google.maps.SymbolPath.CIRCLE,
-        fillColor: 'red',
-        fillOpacity: 1,
-        scale: 4.5,
-        strokeColor: 'white',
-        strokeWeight: 1,
+    setMap(map);
+    renderRoutes(map, routes);
+  }, []);
+
+  const renderRoutes = (map: any, routes: Route[]) => {
+    const bounds = new google.maps.LatLngBounds();
+    const cirlce = {
+      path: google.maps.SymbolPath.CIRCLE,
+      fillColor: 'red',
+      fillOpacity: 1,
+      scale: 10,
+      strokeColor: 'white',
+      strokeWeight: 1,
+    };
+
+    routes.forEach((route, index) => {
+      const from = new google.maps.LatLng(route.path[0].lat, route.path[0].lng);
+      const to = new google.maps.LatLng(route.path[1].lat, route.path[1].lng);
+      const point = {
+        map: map,
+        label: {
+          text: String(index + 1),
+          color: 'white',
+        },
+        icon: cirlce,
       };
 
-      const bounds = new google.maps.LatLngBounds();
-
-      routes.forEach((route) => {
-        const positionFrom = new google.maps.LatLng(
-          route.path[0].lat,
-          route.path[0].lng
-        );
-        new google.maps.Marker({
-          map: map,
-          position: positionFrom,
-          icon: cirlce,
-        });
-        bounds.extend(positionFrom);
-
-        new google.maps.Polyline({
-          map: map,
-          path: route.path,
-          geodesic: true,
-          strokeColor: '#FF0000',
-          strokeOpacity: 1.0,
-          strokeWeight: 2,
-        });
-
-        const positionTo = new google.maps.LatLng(
-          route.path[1].lat,
-          route.path[1].lng
-        );
-        new google.maps.Marker({
-          map: map,
-          position: positionTo,
-          icon: cirlce,
-        });
-
-        bounds.extend(positionTo);
+      new google.maps.Marker({
+        ...point,
+        position: from,
       });
+      bounds.extend(from);
+
+      new google.maps.Polyline({
+        map: map,
+        path: route.path,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+      });
+
+      new google.maps.Marker({
+        ...point,
+        // label: null,
+        position: to,
+      });
+      bounds.extend(to);
 
       if (!bounds.isEmpty()) {
         map.fitBounds(bounds);
       }
+    });
+  };
+
+  useEffect(() => {
+    if (Object.keys(map).length) {
+      renderRoutes(map, routes);
     }
 
-    initMap();
-  });
+    return () => {
+      console.log('clearing data');
+    };
+  }, [routes]);
 
   return (
     <div className="dbg-box">
